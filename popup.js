0a6dc832de6e09a2b019ebd565ec194776037c2d
Ankit <ankit.95sharma@gmail.com>
2023-01-03 23:11:44 +0530

    add permissions and button functionality

--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
+const tabs = await chrome.tabs.query({
+  url: [
+    "https://developer.chrome.com/docs/webstore/*",
+    "https://developer.chrome.com/docs/extensions/*",
+  ],
+});
+
 const collator = new Intl.Collator();
 tabs.sort((a, b) => collator.compare(a.title, b.title));
 
@@ -20,3 +27,10 @@ for (const tab of tabs) {
   elements.add(element);
 }
 document.querySelector("ul").append(...elements);
+
+const button = document.querySelector("button");
+button.addEventListener("click", async () => {
+  const tabIds = tabs.map(({ id }) => id);
+  const group = await chrome.tabs.group({ tabIds });
+  await chrome.tabGroups.update(group, { title: "DOCS" });
+});
